11e8beb0fb7f2b124b89fb251262b17a6a7c28c2
Petri Heinonen <petri@vaadin.com>
2022-04-27 10:51:54 +0300

    Added a more complex Vaadin component (Grid) to the main UI to ensure that even more complex components work).

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -2,6 +2,7 @@ package com.example.application.views.helloworld;
 
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -9,6 +10,8 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import javax.annotation.security.PermitAll;
+import java.util.ArrayList;
+import java.util.List;
 
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
@@ -30,6 +33,52 @@ public class HelloWorldView extends HorizontalLayout {
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
 
         add(name, sayHello);
+
+        Grid<Person> personGrid = new Grid<Person>(Person.class);
+        personGrid.setItems(createPersons());
+        add(personGrid);
+
+    }
+
+    protected List<Person> createPersons(){
+        ArrayList<Person> persons = new ArrayList<>();
+
+        for(int i=0; i<100; i++){
+            persons.add(new Person("First " + i, "Last + i"));
+        }
+
+        return persons;
+    }
+
+
+    public static class Person {
+        private String firstName;
+        private String lastName;
+
+        public Person(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Person() {
+
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
     }
 
 }
